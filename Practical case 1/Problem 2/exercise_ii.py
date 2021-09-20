a8a94ed42f9e550ec1fc8089ab10261d42458396
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-09-20 17:30:26 +0200

    Problem 2 exercise_ii) completed and pylint passed. Now yes, exercise completed

--- Practical case 1/Problem 2/exercise_ii.py
+++ Practical case 1/Problem 2/exercise_ii.py
@@ -12,5 +12,7 @@ if __name__ == "__main__":
     print(Fore.YELLOW, "gcd(20365011074, 12586269023) = ",
           exercise_i.euclides(20365011074, 12586269023))
     print(Fore.YELLOW, "gcd((2**35) - 1, (2**34) - 1) = ",
-          exercise_i.euclides((2 ** 35) - 1, (2 ** 34) - 1))
+          exercise_i.euclides(34359738367, 17179869183))
     sys.exit()
+    # (2**35) - 1 = 34359738367
+    # (2**34) - 1 = 17179869183
