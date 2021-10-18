72ff7ed03622958d0fdd4baa4129d53bd544894c
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-10-18 12:07:54 +0200

    Problem 3 exercise_ii.py and exercise_iii.py completed and pylint passed. Now yes

--- Practical case 1/Problem 3/exercise_iii.py
+++ Practical case 1/Problem 3/exercise_iii.py
@@ -1,16 +1,21 @@
 """
 Authorship: Marc Cervera Rosell
 """
+# pylint: disable=E0401
 import sys
-import exercise_ii
 from colorama import Fore
+import exercise_ii
 
 if __name__ == "__main__":
     print(Fore.BLUE, "The output format is: (gcd, s, t, steps). Remember that as + bt = gcd(a, b)")
-    print(Fore.YELLOW, "XGCD(487669403177, 28736540321) = ", exercise_ii.xgcd(487669403177, 28736540321))
-    print(Fore.YELLOW, "XGCD(20365011074, 12586269025) = ", exercise_ii.xgcd(20365011074, 12586269025))
-    print(Fore.YELLOW, "XGCD(32767, 16383) = ", exercise_ii.xgcd(32767, 16383))
-    print(Fore.YELLOW, "XGCD(167, 94) = ", exercise_ii.xgcd(167, 94))
+    print(Fore.YELLOW, "XGCD(487669403177, 28736540321) = ",
+          exercise_ii.xgcd(487669403177, 28736540321))
+    print(Fore.YELLOW, "XGCD(20365011074, 12586269025) = ",
+          exercise_ii.xgcd(20365011074, 12586269025))
+    print(Fore.YELLOW, "XGCD(32767, 16383) = ",
+          exercise_ii.xgcd(32767, 16383))
+    print(Fore.YELLOW, "XGCD(167, 94) = ",
+          exercise_ii.xgcd(167, 94))
     sys.exit()
     # (2**15) - 1 = 32767
     # (2**34) - 1 = 16383
