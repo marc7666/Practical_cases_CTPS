85b4036e161fe5865597d18b4dcbe5577120f210
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-10-18 12:24:39 +0200

    Exercise 4 done. No pylint

---
+++ Practical case 1/Problem_4/exercise_i_and_ii.py
@@ -0,0 +1,12 @@
+"""
+Authorship: Marc Cervera Rosell
+"""
+import sys
+from colorama import Fore
+import XGCD
+
+if __name__ == "__main__":
+    result1 = XGCD.xgcd(487669403177,28736540321)
+    print(Fore.YELLOW, "The inverse of 28736540321 (mod 487669403177) =", result1[2] % 487669403177)
+    result2 = XGCD.xgcd(20365011074, 12586269025)
+    print(Fore.YELLOW, "The inverse of 12586269025 (mod 20365011074) =", result1[2] % 20365011074)
