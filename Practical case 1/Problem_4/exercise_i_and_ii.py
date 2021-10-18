0f618b35dfcd39fb4438f2606a1ae590970c9032
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-10-18 17:47:31 +0200

    Exercise 5 exercise_i.py done + pylint

--- Practical case 1/Problem_4/exercise_i_and_ii.py
+++ Practical case 1/Problem_4/exercise_i_and_ii.py
@@ -10,3 +10,4 @@ if __name__ == "__main__":
     print(Fore.YELLOW, "The inverse of 28736540321 (mod 487669403177) =", result1[2] % 487669403177)
     result2 = XGCD.xgcd(20365011074, 12586269025)
     print(Fore.YELLOW, "The inverse of 12586269025 (mod 20365011074) =", result1[2] % 20365011074)
+    sys.exit(0)
---
+++ Practical case 1/Problem_5/exercise_i.py
@@ -0,0 +1,49 @@
+"""
+Authorship: Marc Cervera Rosell
+"""
+
+import itertools
+
+from colorama import Fore
+
+
+# pylint: disable=C0200
+def convert_to_z5(numbers):
+    """
+    This method converts all the factors of the polynomial to Z5
+    """
+    result = []
+    for i in range(0, len(numbers)):
+        aux = numbers[i] % 5
+        result.append(aux)
+    return result
+
+
+# The factors will be like: [x,y,z]
+def print_polynomial(factors):
+    """
+    This method convert the list of factors to correspondent polynomial
+    """
+    if len(factors) == 0:
+        return "0"
+
+    polynomial_str = ""
+    converted = convert_to_z5(factors)
+    for i in range(0, len(factors) - 1):
+        if converted[i] == 0:
+            continue
+
+        polynomial_str += str(converted[i])
+        polynomial_str += "x^"
+        power = len(converted) - 1 - i
+        polynomial_str += str(power)
+        polynomial_str += " "
+        polynomial_str += "+"
+        polynomial_str += " "
+    polynomial_str += str(converted[len(converted) - 1])
+    return polynomial_str
+
+
+if __name__ == "__main__":
+    for p in itertools.product(range(5), repeat=3):
+        print(Fore.YELLOW, print_polynomial(p))
